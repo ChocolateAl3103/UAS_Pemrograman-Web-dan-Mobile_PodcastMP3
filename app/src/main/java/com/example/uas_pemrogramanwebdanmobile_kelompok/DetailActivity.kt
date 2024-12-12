a46cdb377b63f51f374679fcb10b0794b775a96f
KomodoBoker <jesslyn2314@gmail.com>
2024-12-12 19:31:14 +0700

    first commit

---
+++ app/src/main/java/com/example/uas_pemrogramanwebdanmobile_kelompok/DetailActivity.kt
@@ -0,0 +1,36 @@
+package com.example.uas_pemrogramanwebdanmobile_kelompok
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.squareup.picasso.Picasso
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var imageView  : ImageView
+    private lateinit var title      : TextView
+    private lateinit var txtdate    : TextView
+    private lateinit var txtdesc    : TextView
+    private lateinit var txtrate    : TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        title       = findViewById(R.id.textJudul)
+        imageView   = findViewById(R.id.imgCourse)
+        txtdate     = findViewById(R.id.textDate)
+        txtdesc     = findViewById(R.id.textDeskripsi)
+        txtrate    = findViewById(R.id.textrate)
+
+        val bundle = intent.getBundleExtra("dataCourse")
+        if (bundle != null) {
+            Picasso.get().load(bundle.getString("image")).into(imageView)
+            title.text      = bundle.getString("title")
+            txtdate.text    = bundle.getString("date")+" s.d "
+            txtdesc.text    = bundle.getString("description")
+            txtrate.text   = "".format(bundle.getString("rate").toString().toFloat())
+        }
+    }
+}
