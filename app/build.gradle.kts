5dc5bd6625793eb8590df255bade11f9f7a26a08
ChocolateAl3103 <chocolateal3103@gmail.com>
2024-12-13 22:05:45 +0700

    First commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.uas_pemrogramanwebdanmobile_kelompok"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.uas_pemrogramanwebdanmobile_kelompok"
